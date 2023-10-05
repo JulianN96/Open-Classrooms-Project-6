c0090bf5466709f0cf52faf1a914c60db6cc291a
Julian Norris <julian.norris96@gmail.com>
2023-10-05 15:47:29 +0200

    Setup structure of React project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './pages/Home';
+import e404 from './components/Error/e404';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>  
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<e404 />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
