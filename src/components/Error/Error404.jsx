eea51193dafa49e583d60db46b7df6ff829267e4
Julian Norris <julian.norris96@gmail.com>
2023-10-05 17:48:35 +0200

    Created and styled 404 page

--- src/components/Error/Error404.jsx
+++ src/components/Error/Error404.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import TopBanner from '../TopBanner'
+import React from 'react';
+import TopBanner from '../TopBanner';
+import { Link } from 'react-router-dom';
+import Footer from '../Footer';
 
 export default function Error404() {
   return (
-    <div>
-        <TopBanner />
-        <h1>404</h1>
+    <div className='error404'>
+      <TopBanner />
+      <div className='error404__container'>
+        <h1 className='error404__404'>404</h1>
+        <p className='error404__text'>
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <Link to='/' className='error404__Link'>
+          Retourner sur la page d'accueil
+        </Link>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
