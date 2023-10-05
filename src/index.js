299f0fda0f6b874e028d6308b7bd316f7f616505
Julian Norris <julian.norris96@gmail.com>
2023-10-05 18:55:08 +0200

    Added about page and stlyed content. No animations yet

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './pages/Home';
 import Error404 from './components/Error/Error404';
+import About from './pages/About';
 
 import Rental from './pages/Rental';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -13,7 +14,7 @@ root.render(
     <Router>  
       <Routes>
         <Route exact path='/' element={<Home />} />
-        <Route path='/about' element ={<about />} />
+        <Route path='/about' element ={<About />} />
         <Route path='/rental/:id' element={<Rental />} />
         <Route path='*' element={<Error404 />} />
       </Routes>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import TopBanner from '../components/TopBanner'
+import Footer from '../components/Footer'
 
-export default function about() {
+export default function About() {
   return (
-    <div>
-      
+    <div className='about'>
+      <TopBanner />
+      <div className='about__hero'/>
+      <div className='aboutInfo__container'>
+        <article className='aboutInfo__dropdown'>
+          <div className='aboutInfo__dropdownBar'>
+            <h2 className='aboutInfo__dropdownBarTitle'>Fiabilité</h2>
+            <svg className='aboutInfo__dropdownBarArrow' xmlns="http://www.w3.org/2000/svg" width="33" height="32" viewBox="0 0 33 32" fill="none">
+              <path d="M15.2897 10.7897C15.9591 10.1202 17.0462 10.1202 17.7157 10.7897L27.9979 21.0719C28.6674 21.7414 28.6674 22.8285 27.9979 23.4979C27.3285 24.1673 26.2414 24.1673 25.572 23.4979L16.5 14.4259L7.42804 23.4926C6.75862 24.162 5.67148 24.162 5.00206 23.4926C4.33265 22.8231 4.33265 21.736 5.00206 21.0666L15.2843 10.7843L15.2897 10.7897Z" fill="white"/>
+            </svg>
+          </div>
+          <div className='aboutInfo__dropdownInfoContainer'><p className='aboutInfo__dropdownInfoText'>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p></div>
+        </article>
+        <article className='aboutInfo__dropdown'>
+          <div className='aboutInfo__dropdownBar'>
+            <h2 className='aboutInfo__dropdownBarTitle'>Respect</h2>
+            <svg className='aboutInfo__dropdownBarArrow' xmlns="http://www.w3.org/2000/svg" width="33" height="32" viewBox="0 0 33 32" fill="none">
+              <path d="M15.2897 10.7897C15.9591 10.1202 17.0462 10.1202 17.7157 10.7897L27.9979 21.0719C28.6674 21.7414 28.6674 22.8285 27.9979 23.4979C27.3285 24.1673 26.2414 24.1673 25.572 23.4979L16.5 14.4259L7.42804 23.4926C6.75862 24.162 5.67148 24.162 5.00206 23.4926C4.33265 22.8231 4.33265 21.736 5.00206 21.0666L15.2843 10.7843L15.2897 10.7897Z" fill="white"/>
+            </svg>
+          </div>
+          <div className='aboutInfo__dropdownInfoContainer'><p className='aboutInfo__dropdownInfoText'>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p></div>
+        </article>
+        <article className='aboutInfo__dropdown'>
+          <div className='aboutInfo__dropdownBar'>
+            <h2 className='aboutInfo__dropdownBarTitle'>Service</h2>
+            <svg className='aboutInfo__dropdownBarArrow' xmlns="http://www.w3.org/2000/svg" width="33" height="32" viewBox="0 0 33 32" fill="none">
+              <path d="M15.2897 10.7897C15.9591 10.1202 17.0462 10.1202 17.7157 10.7897L27.9979 21.0719C28.6674 21.7414 28.6674 22.8285 27.9979 23.4979C27.3285 24.1673 26.2414 24.1673 25.572 23.4979L16.5 14.4259L7.42804 23.4926C6.75862 24.162 5.67148 24.162 5.00206 23.4926C4.33265 22.8231 4.33265 21.736 5.00206 21.0666L15.2843 10.7843L15.2897 10.7897Z" fill="white"/>
+            </svg>
+          </div>
+          <div className='aboutInfo__dropdownInfoContainer'><p className='aboutInfo__dropdownInfoText'>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p></div>
+        </article>
+        <article className='aboutInfo__dropdown'>
+          <div className='aboutInfo__dropdownBar'>
+            <h2 className='aboutInfo__dropdownBarTitle'>Sécurité</h2>
+            <svg className='aboutInfo__dropdownBarArrow' xmlns="http://www.w3.org/2000/svg" width="33" height="32" viewBox="0 0 33 32" fill="none">
+              <path d="M15.2897 10.7897C15.9591 10.1202 17.0462 10.1202 17.7157 10.7897L27.9979 21.0719C28.6674 21.7414 28.6674 22.8285 27.9979 23.4979C27.3285 24.1673 26.2414 24.1673 25.572 23.4979L16.5 14.4259L7.42804 23.4926C6.75862 24.162 5.67148 24.162 5.00206 23.4926C4.33265 22.8231 4.33265 21.736 5.00206 21.0666L15.2843 10.7843L15.2897 10.7897Z" fill="white"/>
+            </svg>
+          </div>
+          <div className='aboutInfo__dropdownInfoContainer'><p className='aboutInfo__dropdownInfoText'>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond qux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos equipes de vérifier que les standards sont bien respectés. Nous organisons également des atelieres sur la sécurité domestique pour nos hôtes.</p></div>
+        </article>
+      </div>
+      <Footer />
     </div>
   )
 }
