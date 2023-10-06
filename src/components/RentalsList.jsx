f4573f78fbab3929d7153d38491986d05b845aa8
Julian Norris <julian.norris96@gmail.com>
2023-10-06 14:03:51 +0200

    Added mapping of data to the homepage's tiles

--- src/components/RentalsList.jsx
+++ src/components/RentalsList.jsx
@@ -1,39 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import testdata from '../data/testdata.json'
 
 export default function RentalsList() {
+
+  const rentalData = testdata
+
+  const rentals = rentalData.map(rental => (
+  <article style={{backgroundImage: `url(${rental.cover})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}} className='rentalsList__tile'>
+   <Link className='rentalsList__Link' to={`/rental/${rental.id}`} >
+   <div className='rentalsList__tileGradient'>
+      <h3 className='rentalsList__tileTitle' >{rental.title}</h3>
+    </div>
+   </Link>
+  </article>
+  ))
+
   return (
     <div className='rentalsList'>
       <div className='rentalsList__container'>
-        <article className='rentalsList__tile'>
-          <div className='rentalsList__tileGradient'>
-            <h3 className='rentalsList__tileTitle'>Titre de la Location</h3>
-          </div>
-        </article>
-        <article className='rentalsList__tile'>
-          <div className='rentalsList__tileGradient'>
-            <h3 className='rentalsList__tileTitle'>Titre de la Location</h3>
-          </div>
-        </article>
-        <article className='rentalsList__tile'>
-          <div className='rentalsList__tileGradient'>
-            <h3 className='rentalsList__tileTitle'>Titre de la Location</h3>
-          </div>
-        </article>
-        <article className='rentalsList__tile'>
-          <div className='rentalsList__tileGradient'>
-            <h3 className='rentalsList__tileTitle'>Titre de la Location</h3>
-          </div>
-        </article>
-        <article className='rentalsList__tile'>
-          <div className='rentalsList__tileGradient'>
-            <h3 className='rentalsList__tileTitle'>Titre de la Location</h3>
-          </div>
-        </article>
-        <article className='rentalsList__tile'>
-          <div className='rentalsList__tileGradient'>
-            <h3 className='rentalsList__tileTitle'>Titre de la Location</h3>
-          </div>
-        </article>
+        {rentals[0]}
+        {rentals[1]}
+        {rentals[2]}
+        {rentals[3]}
+        {rentals[4]}
+        {rentals[5]}
       </div>
     </div>
   );
