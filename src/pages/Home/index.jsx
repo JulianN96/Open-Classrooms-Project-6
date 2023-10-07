83b766b48bacfaa12e0dba6132af21a365297b89
Julian Norris <julian.norris96@gmail.com>
2023-10-07 17:08:54 +0200

    Mobile optimization for homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,13 @@
 import '../../App.css';
 import TopBanner from '../../components/TopBanner';
 import Footer from '../../components/Footer';
-import { useState } from 'react';
 import RentalsList from '../../components/RentalsList';
 
 function App() {
-  const [isHomepageActive, setHomepageActive] = useState(true);
 
   return (
     <div className="homepage">
-      <TopBanner isHomepageActive={isHomepageActive}/>
+      <TopBanner />
       <div className='homepage__hero'>
         <h1 className='homepage__heroTitle'>Chez vous, partout et ailleurs</h1>
       </div>
