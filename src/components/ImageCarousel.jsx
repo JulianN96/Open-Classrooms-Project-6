f11b7f18248e49db408f27f08daf61cc0c09d2a0
Julian Norris <julian.norris96@gmail.com>
2023-10-12 23:09:50 +0200

    Fixed an issue on mobile with the collapse boxes. Content now displays correctly. Added counter to image carousel and updated it to not display controls on single image page

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -5,6 +5,7 @@ export default function ImageCarousel(props) {
   const imageArray = props.pictures;
   let [imageIndex, setImageIndex] = useState(0);
   let [currentImage, setCurrentImage] = useState(imageArray[imageIndex]);
+  console.log(imageArray.length);
 
   function imageFwd() {
     if (
@@ -13,19 +14,15 @@ export default function ImageCarousel(props) {
     ) {
       setImageIndex(0);
     } else if (imageIndex < imageArray.length) {
-      console.log(imageIndex);
       setImageIndex(imageIndex + 1);
-      console.log(imageIndex);
     }
     setCurrentImage(imageArray[imageIndex]);
   }
 
   function imageBack() {
     if (imageIndex === 0 || imageIndex > imageArray.length) {
-      console.log('run1');
       setImageIndex(imageArray.length - 1);
     } else if (imageIndex < 0 || imageIndex < imageArray.length) {
-      console.log('run2');
       setImageIndex(imageIndex - 1);
     }
     setCurrentImage(imageArray[imageIndex]);
@@ -42,34 +39,41 @@ export default function ImageCarousel(props) {
           backgroundPosition: 'center',
         }}
       >
-        <svg
-          className='imageCarousel__prev'
-          onClick={imageBack}
-          xmlns='http://www.w3.org/2000/svg'
-          width='48'
-          height='80'
-          viewBox='0 0 48 80'
-          fill='none'
-        >
-          <path
-            d='M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z'
-            fill='white'
-          />
-        </svg>
-        <svg
-          className='imageCarousel__next'
-          onClick={imageFwd}
-          xmlns='http://www.w3.org/2000/svg'
-          width='48'
-          height='80'
-          viewBox='0 0 48 80'
-          fill='none'
-        >
-          <path
-            d='M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z'
-            fill='white'
-          />
-        </svg>
+        {imageArray.length > 1 && (
+          <div className='imageCarousel__controlsContainer'>
+            <svg
+              className='imageCarousel__prev'
+              onClick={imageBack}
+              xmlns='http://www.w3.org/2000/svg'
+              width='48'
+              height='80'
+              viewBox='0 0 48 80'
+              fill='none'
+            >
+              <path
+                d='M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z'
+                fill='white'
+              />
+            </svg>
+            <div className='imageCarousel__counter'>
+              {imageIndex + 1}/{imageArray.length}
+            </div>
+            <svg
+              className='imageCarousel__next'
+              onClick={imageFwd}
+              xmlns='http://www.w3.org/2000/svg'
+              width='48'
+              height='80'
+              viewBox='0 0 48 80'
+              fill='none'
+            >
+              <path
+                d='M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z'
+                fill='white'
+              />
+            </svg>
+          </div>
+        )}
       </div>
     </div>
   );
